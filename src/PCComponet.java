35dc4dac3a7913efadeddb58509efa3ed47d8089
Juan Carlos <juancarlosjaen.jaeniglesias@gmail.com>
2019-11-21 11:38:31 +0100

    Añadido algunos comentarios y elementos a pc component

--- src/PCComponet.java
+++ src/PCComponet.java
@@ -1,7 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -9,6 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.Set;
 
 public class PCComponet {
 
@@ -34,24 +32,51 @@ public class PCComponet {
 
          WebElement cajaBusqueda = driver.findElement(By.xpath("/html/body/header/div[3]/div[1]/div/div[2]/div/form/input"));
          //el sendKey va a venir del parametro de la interfaz en el imput
-                cajaBusqueda.sendKeys("Mobil");
-                cajaBusqueda.sendKeys(Keys.ENTER);
-         WebDriverWait waiting ;
+                cajaBusqueda.sendKeys("Mobil"+ Keys.ENTER);
+                //cajaBusqueda.sendKeys(Keys.ENTER);
+      /*   WebDriverWait waiting ;
          waiting= new WebDriverWait(driver, 10);
-         waiting.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[class='acc-open-close pull-xs-right']")));
+         waiting.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.acc-block-title:nth-child(9) > a:nth-child(1)")));
 
         WebElement openFiltroSmartPhone =
-                driver.findElement(By.cssSelector("[class='acc-open-close pull-xs-right']"));
+                driver.findElement(By.cssSelector("div.acc-block-title:nth-child(9) > a:nth-child(1)"));
         openFiltroSmartPhone.click();
 
          WebElement selectFiltroSmartPhone =
-                 driver.findElement(By.cssSelector("[data-id='1116']"));
+                 driver.findElement(By.cssSelector("#acc-fil-538 > div:nth-child(1) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1)"));
          selectFiltroSmartPhone.click();
 
+         //Esto viene de starckOverflow para realizar la apertura de la parte de la izquierda, esta sin completar
+         WebDriverWait wait = new WebDriverWait(driver, 10);
+
+//Find frame or iframe and switch
+         wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("iframe-applicationname_ModalDialog_0"));
+
+//Now find the element
+         WebElement el = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//span[@class = 'middle' and contains(text(), 'Next')]")));
+
+//Now click using JavascriptExecutor
+         ((JavascriptExecutor)driver).executeScript("arguments[0].click()" + el);
+
+//Once all your stuff done with this frame need to switch back to default
+         driver.switchTo().defaultContent();
+*/
          List<WebElement> listaElementos =
                  driver.findElements(By.xpath("//*[contains(@class, 'tarjeta-articulo expandible')]"));
          System.out.println("Numero de elementos de la lista: " + listaElementos.size());
 
+         WebElement masElementos =
+                 driver.findElement(By.xpath("//*[@id=\"btnMore\"]"));
+         masElementos.click();
+
+         //Hacer algun tipo de wait
+         List<WebElement> nuevaLista =
+                 driver.findElements(By.xpath("//*[contains(@class, 'tarjeta-articulo expandible')]"));
+         listaElementos.addAll(nuevaLista);
+
+         System.out.println(listaElementos.size());
+
+           // Hay que buscar la forma de cerrar las cookies y la pestaña de suscripcion que aparece y la de blockear notificaciones
 
      }
 
