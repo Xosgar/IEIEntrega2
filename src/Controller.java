3541eaa5f8f1ccfa611e8b401c78f112ea520466
Juan Carlos <juancarlosjaen.jaeniglesias@gmail.com>
2019-11-29 02:19:12 +0100

    añadida interfaz y controlador sin algunas funcionalidades

---
+++ src/Controller.java
@@ -0,0 +1,6 @@
+public class Controller {
+
+    //Falta por crear este controlador, tomara los datos del formulario
+    // y obtendra las listas de los moviles, habrá que tratarls para que no haya repetidos y demás
+    //redirigirá a la pagina de Moviles.html
+}
